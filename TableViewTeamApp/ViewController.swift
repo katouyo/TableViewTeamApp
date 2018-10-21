cfd14547cddd337ce8e739c9794b544847ced204
katouyo <yothiac13@gmail.com>
2018-10-21 13:08:56 +0900

    Initial Commit

---
+++ TableViewTeamApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewTeamApp
+//
+//  Created by Yoshiaki Kato on 2018/10/21.
+//  Copyright © 2018 Yoshiaki Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
